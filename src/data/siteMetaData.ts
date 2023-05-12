0e398c439fd56a0e1535d0a6f741f16baf7004f6
johnkdunyo <johnkdunyo@gmail.com>
2023-05-12 11:42:23 +0000

    added site mete data

---
+++ src/data/siteMetaData.ts
@@ -0,0 +1,29 @@
+const siteMetadata = {
+  title: 'John Kwame Dunyo – full-stack engineer, system engineer and product developer',
+  author: 'John Kwame Dunyo',
+  headerTitle: "John KWame's Portfolio",
+  description:
+    "I'm John Kwame Dunyo. I'm a software devloper, systems engineer and product developer. Welcome to my corner of the internet. I'm glad you're here!",
+  language: 'en-us',
+  siteUrl: '',
+  siteRepo: 'https://github.com/johnkdunyo/johnkdunyo.dev',
+  avatarImage:
+    'https://res.cloudinary.com/braydoncoyer/image/upload/v1636553710/avatar.jpg',
+  socialBanner:
+    'https://res.cloudinary.com/braydoncoyer/image/upload/v1638884997/default-social-image.png',
+  email: 'mailto:johnkdunyo@gmail.com',
+  github: 'https://github.com/johnkdunyo',
+  twitter: '',
+  twitterHandle: '',
+  linkedin: '',
+  newsletter: '',
+  resume: '',
+  snippets:
+    'https://www.notion.so/9f35162787e0448094188b66304cf9f6?v=1b9c4e146edf46148fe7b1c5346cbea4',
+  locale: 'en-US',
+  analytics: {
+    plausibleDataDomain: 'johnkdunyo.dev'
+  }
+};
+
+export default siteMetadata
