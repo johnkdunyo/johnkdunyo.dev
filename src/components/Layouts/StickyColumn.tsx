9a19efb478189daa8bbcec64a57602a2e39ff796
johnkdunyo <johnkdunyo@gmail.com>
2023-05-12 11:44:03 +0000

    added sticky column

---
+++ src/components/Layouts/StickyColumn.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+interface StickyColumnProps {
+  children: ReactNode;
+}
+
+export function StickyColumn({ children }: StickyColumnProps) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-12 md:gap-8">
+      <div className="col-span-8">{children}</div>
+      <aside className="hidden lg:inline-block md:sticky md:top-[365px] md:self-start col-span-4 space-y-8"></aside>
+    </div>
+  );
+}
